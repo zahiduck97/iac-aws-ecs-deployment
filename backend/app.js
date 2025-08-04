bee75e962ec017724fb8b26771286b6fce8ac795
Zahiduck <zahid.butron@gmail.com>
2025-08-04 13:47:34 -0600

    Listen App in the port 80

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 app.get('/', (req, res) => {
     const htmlPath = path.join(__dirname, 'landing.html');
